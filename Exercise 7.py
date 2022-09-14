487a91b61582d0cb2fce238fbcbff8794256a13c
MKRadius <caomanhkhai110@gmail.com>
2022-09-15 01:55:23 +0300

    Exercise

--- Exercise 7.py
+++ Exercise 7.py
@@ -6,7 +6,7 @@ userMonth = int(input("Input month (1 - 12): "))
 userSeason = seasonOfTheYear[userMonth // 3 - 1]
 
 print(f"Month {userMonth} is {userSeason}")
-"""
+
 
 #Exercise 7.2
 name = set()
@@ -27,4 +27,49 @@ print("Name list:")
 
 for i in name:
     print(i)
+"""   
+
+
+#Exercise 7.3
+airport = dict()
+
+def enterNewAirport(airport):
+    inputICAO = str(input("Insert airport ICAO code: "))
+    inputName = str(input("Insert airport name: "))
+
+    airport[inputICAO] = inputName
+    print("Airport successfully added!\n")
     
+    return airport
+
+def fetchExistingAirport(airport):
+    inputICAO = str(input("Insert airport ICAO code : "))
+    
+    if inputICAO in airport:
+        print(f"Airport name: {airport[inputICAO]}\n")
+    else:
+        print("Airport not found.\n")
+    
+    return airport
+
+while True:
+    print("(1) Enter new airport")
+    print("(2) Fetch existing airport's information")
+    print("(3) Quit")
+
+    userInput = input("Choose your option (1/2/3): ")
+    print("")
+
+    if userInput == "1":
+        enterNewAirport(airport)
+    elif userInput == "2":
+        fetchExistingAirport(airport)
+    elif userInput == "3":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid input. Please try again.\n")
+        continue
+
+#Dicttionary check
+#print(airport)
