e8e38e71b555692b95c106c789db799a87a48961
MKRadius <caomanhkhai110@gmail.com>
2022-09-17 16:37:40 +0300

    Exercise

--- Exercise 8.py
+++ Exercise 8.py
@@ -46,4 +46,29 @@ if cursor.rowcount > 0:
 
 
 #Exercise 8.3
+from geopy import distance
 
+airportName = ["", ""]
+airportPos = [[0, 0], [0, 0]]
+
+for i in range(2):
+    airportCode = input(f"Input ICAO code of airport {i + 1}: ")
+
+    sql = "select name, latitude_deg, longitude_deg from airport "
+    sql += "where ident = '" + airportCode + "'"
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    airportName[i] = result[0][0]
+    airportPos[i][0] = result[0][1]
+    airportPos[i][1] = result[0][2]
+
+    #print latitude and longitude of airport
+    #print(airportPos[i][0])
+    #print(airportPos[i][1])
+
+
+print(f"Distance between {airportName[0]} and {airportName[1]} is ", end = '')
+print(f"{distance.distance(airportPos[0], airportPos[1]).km:.3f} km")
