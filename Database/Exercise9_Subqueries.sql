de80b95ae3bc1897dc2dadf7a69489a1f346dc78
MKRadius <caomanhkhai110@gmail.com>
2022-09-20 02:21:51 +0300

    Exercise

--- Database/Exercise9_Subqueries.sql
+++ Database/Exercise9_Subqueries.sql
@@ -16,3 +16,20 @@ where iso_country = (
     from country
     where name = "Monaco"
 );
+
+
+/*3*/
+select screen_name
+from game
+where id = (
+    select game_id
+    from goal_reached
+    where goal_id = (
+        select name
+        from goal
+        where name = "CLOUDS"
+    )
+);
+
+
+/*3*/
