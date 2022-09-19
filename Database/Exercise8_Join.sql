7ca3548332c606c226e13472e50f2d381994c205
MKRadius <caomanhkhai110@gmail.com>
2022-09-20 01:24:05 +0300

    Exercise

--- Database/Exercise8_Join.sql
+++ Database/Exercise8_Join.sql
@@ -4,3 +4,10 @@ from country
 inner join airport 
 on country.iso_country = airport.iso_country 
 where airport.scheduled_service = "yes" and country.name = "Finland";
+
+
+/*2*/
+select game.screen_name as screen_name, airport.name as name
+from game
+inner join airport
+on game.location = airport.ident;
