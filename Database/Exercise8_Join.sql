b00cdbe71a4a8b4261b792b670c7366e5d3d5d80
MKRadius <caomanhkhai110@gmail.com>
2022-09-20 01:20:48 +0300

    Exercise

--- Database/Exercise8_Join.sql
+++ Database/Exercise8_Join.sql
@@ -1,5 +1,6 @@
 /*1*/
 select country.name as "country name", airport.name as "airport name"
 from country
-inner join airport
+inner join airport 
+on country.iso_country = airport.iso_country 
 where airport.scheduled_service = "yes" and country.name = "Finland";
