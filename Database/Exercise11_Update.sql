f0686af1ba094559519745cde9b73ead43f5b8d6
MKRadius <caomanhkhai110@gmail.com>
2022-09-26 03:57:07 +0300

    Exercise

--- Database/Exercise11_Update.sql
+++ Database/Exercise11_Update.sql
@@ -1,9 +1,9 @@
 update game
-set screen_name = "Vesa"
-where location in (
+set location = (
     select ident 
     from airport
     where name = "Nottingham Airport"
-);
+), co2_consumed = co2_consumed + 500
+where screen_name = "Vesa";
 
 
