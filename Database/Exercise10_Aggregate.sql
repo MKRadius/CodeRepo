2d4a529691d8c3073704c49c5f754a47f409d5a7
MKRadius <caomanhkhai110@gmail.com>
2022-09-26 03:13:30 +0300

    Exercise

--- Database/Exercise10_Aggregate.sql
+++ Database/Exercise10_Aggregate.sql
@@ -13,3 +13,12 @@ select screen_name, count(*)
 from game, goal_reached
 where id = game_id
 group by screen_name;
+
+
+
+select screen_name
+from game
+where co2_consumed in (
+    select min(co2_consumed)
+    from game
+)
