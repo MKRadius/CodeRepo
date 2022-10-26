304a7b3ee2b2dfb862440f849cf4d1f8e08c5112
MKRadius <caomanhkhai110@gmail.com>
2022-10-26 13:13:10 +0300

    Exercise

--- Exercise 10.py
+++ Exercise 10.py
@@ -1,4 +1,4 @@
-#Exercise 10.1
+"""#Exercise 10.1
 class Elevator:
     def __init__(self, floor = 0):
         self.elevator_floor = floor
@@ -30,6 +30,42 @@ print("Arrived at", h.elevator_floor)
 h.go_to_floor(0)
 print("Arrived at", h.elevator_floor)
 
-
+"""
 
 #Exercise 10.2
+from math import floor
+
+
+class Elevator:
+    def __init__(self, floor = 0):
+        self.elevator_floor = floor
+
+    def floor_up(self):
+        self.elevator_floor += 1
+        print("At floor", self.elevator_floor)
+
+    def floor_down(self):
+        self.elevator_floor -= 1
+        print("At floor", self.elevator_floor)
+
+    def go_to_floor(self, desired_floor):
+        if self.elevator_floor != desired_floor:
+            while self.elevator_floor != desired_floor:
+                if self.elevator_floor > desired_floor:
+                    self.floor_down()
+                elif self.elevator_floor < desired_floor:
+                    self.floor_up()
+        else:
+            print("Already at floor", desired_floor)
+
+class Building:
+    def __init__(self, bottom_floor, top_floor, elevator_count = 3):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.elevator_count = elevator_count
+        self.elevators = []
+    
+    def run_elevator():
+        pass
+
+
